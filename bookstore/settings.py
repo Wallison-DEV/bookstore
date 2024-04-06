b2d3d83cb09ff2b77b9f0a1f26b954e065286e21
Wallison <wallisonzwka@gmail.com>
2024-04-06 17:53:33 -0300

    feature/workflows

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -107,8 +107,8 @@ INTERNAL_IPS = {
     '127.0.0.1',
 }
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+# DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(' ')
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(' ')
