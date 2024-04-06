9b898cfb1fe236615a7dafed6809b3bc145eb26b
Wallison <wallisonzwka@gmail.com>
2024-04-06 15:53:08 -0300

    feature/gha

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,13 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 from product.models import Product
 from product.serializers import ProductSerializer
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
     
     def get_queryset(self):
